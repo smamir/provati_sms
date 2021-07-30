5e84deaf0bfc26fb0bdb315bc2681e7f87f42db3
Amir <smamir.tuhin@gmail.com>
2021-07-30 21:31:45 +0600

    Auto stash before checking out "HEAD"

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -27,14 +27,14 @@ class SettingsTableSeeder extends Seeder
             ['type' => 'alt_email', 'description' => ''],
             ['type' => 'email_host', 'description' => ''],
             ['type' => 'email_pass', 'description' => ''],
-            ['type' => 'lock_exam', 'description' => 0],
-            ['type' => 'logo', 'description' => ''],
-            ['type' => 'next_term_fees_j', 'description' => '20000'],
-            ['type' => 'next_term_fees_pn', 'description' => '25000'],
-            ['type' => 'next_term_fees_p', 'description' => '25000'],
-            ['type' => 'next_term_fees_n', 'description' => '25600'],
-            ['type' => 'next_term_fees_s', 'description' => '15600'],
-            ['type' => 'next_term_fees_c', 'description' => '1600'],
+//            ['type' => 'lock_exam', 'description' => 0],
+//            ['type' => 'logo', 'description' => ''],
+//            ['type' => 'next_term_fees_j', 'description' => '20000'],
+//            ['type' => 'next_term_fees_pn', 'description' => '25000'],
+//            ['type' => 'next_term_fees_p', 'description' => '25000'],
+//            ['type' => 'next_term_fees_n', 'description' => '25600'],
+//            ['type' => 'next_term_fees_s', 'description' => '15600'],
+//            ['type' => 'next_term_fees_c', 'description' => '1600'],
         ];
 
         DB::table('settings')->insert($data);
