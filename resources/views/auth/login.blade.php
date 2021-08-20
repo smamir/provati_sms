3a5ec33aa0ebac53064c3aaae126a90444779023
Amir <smamir.tuhin@gmail.com>
2021-08-21 02:25:45 +0600

    minor login page fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,7 @@
 
 
                             <div class="form-group ">
-                                <input type="text" class="form-control" name="identity" value="{{ old('identity') }}" placeholder="Login ID or Email">
+                                <input type="text" class="form-control" name="identity" value="{{ old('identity') }}" placeholder="Email">
                             </div>
 
                             <div class="form-group ">
